037cbf7ed8831edd7676579e8bcb4492b954a355
kjmoran598 <kmoran598@gmail.com>
2024-04-26 12:22:28 -0700

    Middle of class

--- src/Basics.java
+++ src/Basics.java
@@ -1,6 +1,11 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+
+import org.testng.Assert;
+
 import files.payload;
 
 public class Basics {
@@ -12,18 +17,47 @@ public class Basics {
 		//When - Submit the API (resource and http method)
 		//Then - Validate the response
 		
+		//Add place, update place with new address, get place and validate Place ID
+		
 		RestAssured.baseURI = "https://rahulshettyacademy.com/";
-		given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
+		String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
 		.body(payload.AddPlace())
 		.when().post("maps/api/place/add/json")
 		.then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
+		.header("Server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
+		
+		System.out.println(response);
+		
+		JsonPath js = new JsonPath(response);
+		String placeID = js.get("place_id");
+		
+		//Update place with new address from placeID
+		String newAddress = "555 Burrard St";
+		
+		given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
+		.body("{\n"
+				+ "\"place_id\":\""+ placeID + "\",\n"
+				+ "\"address\":\"" + newAddress + "\",\n"
+				+ "\"key\":\"qaclick123\"\n"
+				+ "}")
+		.when().put("maps/api/place/update/json")
+		.then().log().all().assertThat().statusCode(200).body("msg", equalTo("Address successfully updated"))
 		.header("Server", "Apache/2.4.52 (Ubuntu)");
 		
-		//Add place, update place with new address, get place and validate Place ID
+		//Use GET API to verify address matches the new address
 		
-		//Add place, update place with new address, get place and validate Place ID
+		String verify = given().log().all().queryParam("key", "qaclick123").queryParam("place_id", placeID).header("Content-Type","application/json")
+		.body("")
+		.when().get("maps/api/place/get/json")
+		.then().log().all().assertThat().statusCode(200).extract().response().asString();
 		
-		//Moran is the best
+		JsonPath jsv = new JsonPath(verify);
+		
+		String actualAddress = jsv.get("address");
+			
+		Assert.assertEquals(newAddress, actualAddress);
+
+		
 
 	}
 
